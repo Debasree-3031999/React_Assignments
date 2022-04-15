c6b706ecf3ee5742cad4db1f84b042d5a6759b78
Debasree-3031999 <debasreeghosh1999@gmail.com>
2022-04-15 16:52:52 +0530

    day 2 counter done

---
+++ day2/my-app/src/components/counter.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+const Counter=()=>{
+    const[counter,setCounter]=useState(0);
+    const handleChange=(num)=>{
+        setCounter(counter+num)
+    }
+    return(
+        <>
+            <div>
+               <h3 className={`${counter%2===0 ? "greentxt":"redtxt"}`}>Counter : {counter}</h3>
+               <button onClick={()=>{
+                   handleChange(1)
+               }}>Increment(+1)</button>
+               <button onClick={()=>{
+                   handleChange(-1)
+               }}>Decrement(-1)</button>
+               <button onClick={()=>{
+                   handleChange(counter)
+               }}>Double</button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
